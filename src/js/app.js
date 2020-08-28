1d824657ccaf78729260310e8c493078ac4a781d
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-28 13:15:14 +0300

    Completed all task

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,10 @@
 const alarmColor = function (profile) {
   const { health } = profile;
   if (health < 100 && health > 30) {
-    console.log('wounded');
     return 'wounded';
   } if (health <= 30) {
-    console.log('critical');
     return 'critical';
   }
-  console.log('healthy');
   return 'healthy';
 };
 
